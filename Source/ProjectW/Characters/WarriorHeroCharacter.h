3f7a9dab373a7fb81396d07d5dc01ac59bf05777
DESKTOP-OAPN4UI\Gunn <gunn0467@gmail.com>
2025-03-08 04:26:25 +0900

    [gunn] Set up hero camera

--- Source/ProjectW/Characters/WarriorHeroCharacter.h
+++ Source/ProjectW/Characters/WarriorHeroCharacter.h
@@ -7,12 +7,26 @@
 #include "WarriorBaseCharacter.h"
 #include "WarriorHeroCharacter.generated.h"
 
-/**
- * 
- */
+class USpringArmComponent;
+class UCameraComponent;
+
 UCLASS()
 class PROJECTW_API AWarriorHeroCharacter : public AWarriorBaseCharacter
 {
 	GENERATED_BODY()
+
+public:
+	AWarriorHeroCharacter();
+
+protected:
+	virtual void BeginPlay() override;
+
+private:
+#pragma  region Components
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="Camera", meta=(AllowPrivateAccess="true"))
+	TObjectPtr<USpringArmComponent> CameraBoom = nullptr;
 	
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="Camera", meta=(AllowPrivateAccess="true"))
+	TObjectPtr<UCameraComponent> FollowCamera = nullptr;
+#pragma endregion 
 };
