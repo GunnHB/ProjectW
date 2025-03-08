b46a345248ac67c888288c1d99c5268453f0951a
DESKTOP-OAPN4UI\Gunn <gunn0467@gmail.com>
2025-03-08 15:43:47 +0900

    [gunn] hero anim instance

--- Source/ProjectW/AnimInstance/Hero/WarriorHeroAnimInstance.h
+++ Source/ProjectW/AnimInstance/Hero/WarriorHeroAnimInstance.h
@@ -6,6 +6,7 @@
 #include "../WarriorCharacterAnimInstance.h"
 #include "WarriorHeroAnimInstance.generated.h"
 
+class AWarriorHeroCharacter;
 /**
  * 
  */
@@ -13,5 +14,21 @@ UCLASS()
 class PROJECTW_API UWarriorHeroAnimInstance : public UWarriorCharacterAnimInstance
 {
 	GENERATED_BODY()
-	
+
+public:
+	virtual void NativeInitializeAnimation() override;
+	virtual void NativeThreadSafeUpdateAnimation(float DeltaSeconds) override;
+
+protected:
+	UPROPERTY(VisibleDefaultsOnly, BlueprintReadOnly, Category="AnimData|References")
+	TObjectPtr<AWarriorHeroCharacter> OwningHeroCharacter = nullptr;
+
+	UPROPERTY(VisibleDefaultsOnly, BlueprintReadOnly, Category="AnimData|LocomotionData")
+	bool bShouldEnterRelaxState = false;
+
+	// 휴식 상태로 전환되는 임계값
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="AnimData|LocomotionData")
+	float EnterRelaxStateThreshold = 0.f;
+
+	float IdleElapsedTime = 0.f;
 };
