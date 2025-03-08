7a5ee6ad1558c3fa966bf2acf5d97a7c2a56c438
DESKTOP-OAPN4UI\Gunn <gunn0467@gmail.com>
2025-03-08 11:17:04 +0900

    [gunn] native gameplay tags

---
+++ Source/ProjectW/WarriorGameplayTags.h
@@ -0,0 +1,11 @@
+﻿
+#pragma once
+
+#include "NativeGameplayTags.h"
+
+namespace WarriorGameplayTags
+{
+    /* Input Tags */
+    PROJECTW_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(InputTag_Move)
+    PROJECTW_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(InputTag_Look)
+}
