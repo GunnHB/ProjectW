4cfa5afd8721bb778a875ac32b64e57ed450d0f5
DESKTOP-OAPN4UI\Gunn <gunn0467@gmail.com>
2025-04-01 19:13:51 +0900

    [gunn] register spawned weapon

--- Source/ProjectW/Components/Combat/PawnCombatComponent.h
+++ Source/ProjectW/Components/Combat/PawnCombatComponent.h
@@ -3,10 +3,12 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "GameplayTagContainer.h"
 #include "../PawnExtensionComponentBase.h"
 
 #include "PawnCombatComponent.generated.h"
 
+class AWarriorWeaponBase;
 /**
  * 
  */
@@ -14,5 +16,20 @@ UCLASS()
 class PROJECTW_API UPawnCombatComponent : public UPawnExtensionComponentBase
 {
 	GENERATED_BODY()
+
+public:
+	UFUNCTION(BlueprintCallable, Category="Warrior|Combat")
+	void RegisterSpawnedWeapon(FGameplayTag InWeaponTagToRegister, AWarriorWeaponBase* InWeaponToRegister, bool bRegisterAsEquippedWeapon = false);
+
+	UFUNCTION(BlueprintCallable, Category="Warrior|Combat")
+	AWarriorWeaponBase* GetCharacterCarriedWeaponByTag(FGameplayTag InWeaponTagToGet) const;
+
+	UPROPERTY(BlueprintReadWrite, Category="Warrior|Combat")
+	FGameplayTag CurrentEquippedWeaponTag;
+
+	UFUNCTION(BlueprintCallable, Category="Warrior|Combat")
+	AWarriorWeaponBase* GetCharacterCurrentEquippedWeapon() const;
 	
+private:
+	TMap<FGameplayTag, AWarriorWeaponBase*> CharacterCarriedWeaponMap;
 };
--- Source/ProjectW/ProjectWGameplayTags.h
+++ Source/ProjectW/ProjectWGameplayTags.h
@@ -8,4 +8,7 @@ namespace ProjectWGameplayTags
     /* Input Tags */
     PROJECTW_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(InputTag_Move)
     PROJECTW_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(InputTag_Look)
+
+    /* Player Tags */
+    PROJECTW_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Player_Weapon_Axe)
 }
