d95028258125e47faf664d7d672a91b497b6f76f
DESKTOP-OAPN4UI\Gunn <gunn0467@gmail.com>
2025-06-24 22:03:01 +0900

    [ProjectW][set_up_hero_character] character anim instance
    (cherry picked from commit 4e73d43bb90c6bf8cf6c18c63aaff2cd0e8fd3ee)

---
+++ Source/ProjectW/AnimInstances/Hero/WarriorHeroLinkedAnimLayer.h
@@ -0,0 +1,18 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "../WarriorBaseAnimInstance.h"
+
+#include "WarriorHeroLinkedAnimLayer.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTW_API UWarriorHeroLinkedAnimLayer : public UWarriorBaseAnimInstance
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/ProjectW/AnimInstances/WarriorCharacterAnimInstance.h
@@ -0,0 +1,36 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "WarriorBaseAnimInstance.h"
+#include "WarriorCharacterAnimInstance.generated.h"
+
+class UCharacterMovementComponent;
+class AWarriorBaseCharacter;
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTW_API UWarriorCharacterAnimInstance : public UWarriorBaseAnimInstance
+{
+	GENERATED_BODY()
+
+public:
+	virtual void NativeInitializeAnimation() override;
+	virtual void NativeThreadSafeUpdateAnimation(float DeltaSeconds) override;
+
+protected:
+	UPROPERTY()
+	TObjectPtr<AWarriorBaseCharacter> OwningCharacter = nullptr;
+
+	UPROPERTY()
+	TObjectPtr<UCharacterMovementComponent> OwningMovementComponent = nullptr;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="AnimData|LocomotionData")
+	float GroundSpeed = 0.f;
+	
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="AnimData|LocomotionData")
+	uint8 bHasAcceleration:1 = false;
+};
