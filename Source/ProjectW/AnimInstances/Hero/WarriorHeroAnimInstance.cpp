7e8614e95a617fa179f7ce6a7c5e1d31b6991fb3
DESKTOP-OAPN4UI\Gunn <gunn0467@gmail.com>
2025-06-24 22:51:15 +0900

    [ProjectW][set_up_hero_character] hero anim instance
    (cherry picked from commit 69a464b8ad316bde5ea6d0676c301ff17600f3a8)

--- Source/ProjectW/AnimInstances/Hero/WarriorHeroAnimInstance.cpp
+++ Source/ProjectW/AnimInstances/Hero/WarriorHeroAnimInstance.cpp
@@ -3,3 +3,29 @@
 
 #include "WarriorHeroAnimInstance.h"
 
+#include "ProjectW/Characters/WarriorBaseCharacter.h"
+#include "ProjectW/Characters/WarriorHeroCharacter.h"
+
+void UWarriorHeroAnimInstance::NativeInitializeAnimation()
+{
+	Super::NativeInitializeAnimation();
+
+	if (IsValid(OwningCharacter))
+		OwningHeroCharacter = Cast<AWarriorHeroCharacter>(OwningCharacter);
+}
+
+void UWarriorHeroAnimInstance::NativeThreadSafeUpdateAnimation(float DeltaSeconds)
+{
+	Super::NativeThreadSafeUpdateAnimation(DeltaSeconds);
+
+	if (bHasAcceleration)
+	{
+		IdleElapsedTime = 0.f;
+		bShouldEnterRelaxState = false;
+	}
+	else
+	{
+		IdleElapsedTime += DeltaSeconds;
+		bShouldEnterRelaxState = (IdleElapsedTime >= EnterRelaxStateThreshold);
+	}
+}
--- Source/ProjectW/AnimInstances/Hero/WarriorHeroAnimInstance.h
+++ Source/ProjectW/AnimInstances/Hero/WarriorHeroAnimInstance.h
@@ -6,6 +6,7 @@
 #include "../WarriorCharacterAnimInstance.h"
 #include "WarriorHeroAnimInstance.generated.h"
 
+class AWarriorHeroCharacter;
 /**
  * 
  */
@@ -13,5 +14,21 @@ UCLASS()
 class PROJECTW_API UWarriorHeroAnimInstance : public UWarriorCharacterAnimInstance
 {
 	GENERATED_BODY()
-	
+
+public:
+	virtual void NativeInitializeAnimation() override;
+	virtual void NativeThreadSafeUpdateAnimation(float DeltaSeconds) override;
+
+protected:
+	UPROPERTY(VisibleDefaultsOnly, BlueprintReadOnly, Category="AnimData|References")
+	TObjectPtr<AWarriorHeroCharacter> OwningHeroCharacter = nullptr;
+
+	UPROPERTY(VisibleDefaultsOnly, BlueprintReadOnly, Category="AnimData|LocomotionData")
+	uint8 bShouldEnterRelaxState:1 = false;
+
+	// Relax 상태로의 전환에 걸리는 시간
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="AnimData|LocomotionData")
+	float EnterRelaxStateThreshold = 5.f;
+
+	float IdleElapsedTime = 0.f;
 };
