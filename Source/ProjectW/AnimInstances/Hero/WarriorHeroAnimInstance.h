4e73d43bb90c6bf8cf6c18c63aaff2cd0e8fd3ee
DESKTOP-OAPN4UI\Gunn <gunn0467@gmail.com>
2025-06-24 22:03:01 +0900

    [ProjectW][set_up_hero_character] character anim instance

---
+++ Source/ProjectW/AnimInstances/Hero/WarriorHeroAnimInstance.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "../WarriorCharacterAnimInstance.h"
+#include "WarriorHeroAnimInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTW_API UWarriorHeroAnimInstance : public UWarriorCharacterAnimInstance
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/ProjectW/AnimInstances/WarriorCharacterAnimInstance.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "WarriorCharacterAnimInstance.h"
+
+#include "GameFramework/CharacterMovementComponent.h"
+
+#include "ProjectW/Characters/WarriorBaseCharacter.h"
+
+void UWarriorCharacterAnimInstance::NativeInitializeAnimation()
+{
+	OwningCharacter = Cast<AWarriorBaseCharacter>(TryGetPawnOwner());
+	if (IsValid(OwningCharacter) != false)
+		OwningMovementComponent = OwningCharacter->GetCharacterMovement();
+}
+
+void UWarriorCharacterAnimInstance::NativeThreadSafeUpdateAnimation(float DeltaSeconds)
+{
+	// 애니메이션 쓰레드에서 동작
+	// NativeUpdateAnimation에서 설정한 정보를 처리하여 애니메이션을 설정
+
+	if (IsValid(OwningCharacter) == false || IsValid(OwningMovementComponent) == false)
+		return;
+
+	GroundSpeed = OwningCharacter->GetVelocity().Size2D();
+	bHasAcceleration = OwningMovementComponent->GetCurrentAcceleration().SizeSquared2D() > 0.f;
+}
