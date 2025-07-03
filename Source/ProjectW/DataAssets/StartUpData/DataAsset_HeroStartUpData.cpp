86d2a14c3a5baf7df9c5a9f4553510a730ea52d3
DESKTOP-OAPN4UI\Gunn <gunn0467@gmail.com>
2025-07-03 23:28:03 +0900

    [ProjectW][melee_combo_system] hero ability set

--- Source/ProjectW/DataAssets/StartUpData/DataAsset_HeroStartUpData.cpp
+++ Source/ProjectW/DataAssets/StartUpData/DataAsset_HeroStartUpData.cpp
@@ -3,3 +3,28 @@
 
 #include "DataAsset_HeroStartUpData.h"
 
+#include "ProjectW/AbilitySystem/WarriorAbilitySystemComponent.h"
+#include "ProjectW/AbilitySystem/Abilities/WarriorGameplayAbility.h"
+
+bool FWarriorHeroAbilitySet::IsValid() const
+{
+	return InputTag.IsValid() && AbilityToGrant;
+}
+
+void UDataAsset_HeroStartUpData::GiveToAbilitySystemComponent(UWarriorAbilitySystemComponent* InASCToGive, int32 ApplyLevel)
+{
+	Super::GiveToAbilitySystemComponent(InASCToGive, ApplyLevel);
+
+	for (const FWarriorHeroAbilitySet& AbilitySet : HeroStartUpAbilitySets)
+	{
+		if (AbilitySet.IsValid() == false)
+			continue;
+
+		FGameplayAbilitySpec AbilitySpec(AbilitySet.AbilityToGrant);
+		AbilitySpec.SourceObject = InASCToGive->GetAvatarActor();
+		AbilitySpec.Level = ApplyLevel;
+		AbilitySpec.DynamicAbilityTags.AddTag(AbilitySet.InputTag);
+
+		InASCToGive->GiveAbility(AbilitySpec);
+	}
+}
