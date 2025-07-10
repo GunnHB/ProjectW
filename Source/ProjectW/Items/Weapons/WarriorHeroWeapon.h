913a456900f91526da5da399e8ca8ef9317e9ac1
DESKTOP-OAPN4UI\Gunn <gunn0467@gmail.com>
2025-07-10 23:47:10 +0900

    [ProjectW][melee_combo_system] handle unequip axe

--- Source/ProjectW/Items/Weapons/WarriorHeroWeapon.h
+++ Source/ProjectW/Items/Weapons/WarriorHeroWeapon.h
@@ -3,6 +3,7 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "GameplayAbilitySpecHandle.h"
 #include "WarriorWeaponBase.h"
 #include "ProjectW/WarriorTypes/WarriorStructTypes.h"
 #include "WarriorHeroWeapon.generated.h"
@@ -18,4 +19,13 @@ class PROJECTW_API AWarriorHeroWeapon : public AWarriorWeaponBase
 public:
 	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="WeaponData")
 	FWarriorHeroWeaponData HeroWeaponData;
+
+	UFUNCTION(BlueprintCallable)
+	void AssignGrantedAbilitySpecHandles(const TArray<FGameplayAbilitySpecHandle>& InSpecHandles);
+
+	UFUNCTION(BlueprintPure)
+	TArray<FGameplayAbilitySpecHandle> GetGrantedAbilitySpecHandles() const;
+
+private:
+	TArray<FGameplayAbilitySpecHandle> GrantedAbilitySpecHandles;
 };
