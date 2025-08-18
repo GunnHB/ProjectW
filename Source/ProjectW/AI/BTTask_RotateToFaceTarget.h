a0e46ce08ac35e36d12d45c1cd0a0654c9403837
DESKTOP-OAPN4UI\Gunn <gunn0467@gmail.com>
2025-08-18 16:18:33 +0900

    [ProjectW][enemy_ai] rotate enemy in task

--- Source/ProjectW/AI/BTTask_RotateToFaceTarget.h
+++ Source/ProjectW/AI/BTTask_RotateToFaceTarget.h
@@ -39,6 +39,11 @@ public:
 	virtual uint16 GetInstanceMemorySize() const override;
 	virtual FString GetStaticDescription() const override;
 	// ~ End UBTNode Interface
+	
+	virtual EBTNodeResult::Type ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) override;
+	virtual void TickTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory, float DeltaSeconds) override;
+
+	bool HasReachedAnglePercision(APawn* QueryPawn, AActor* TargetActor) const;
 
 	UPROPERTY(EditAnywhere, Category="Face Target")
 	float AnglePrecision = 0.f;
