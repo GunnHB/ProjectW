7cce2467339004dc1463d00de9b2a213397526bd
DESKTOP-OAPN4UI\Gunn <gunn0467@gmail.com>
2025-09-12 23:14:15 +0900

    [ProjectW][hero_combat_abilities] get value at level

--- Source/ProjectW/WarriorFunctionLibrary.h
+++ Source/ProjectW/WarriorFunctionLibrary.h
@@ -8,6 +8,8 @@
 #include "WarriorTypes/WarriorEnumTypes.h"
 #include "WarriorFunctionLibrary.generated.h"
 
+struct FScalableFloat;
+
 class UPawnCombatComponent;
 class UWarriorAbilitySystemComponent;
 /**
@@ -39,4 +41,7 @@ public:
 
 	UFUNCTION(BlueprintPure, Category="Warrior|FunctionLibrary")
 	static bool IsTargetPawnHostile(APawn* QueryPawn, APawn* TargetPawn);
+
+	UFUNCTION(BlueprintPure, Category="Warrior|FunctionLibrary", meta=(CompactNodeTitle="Get Value At Level"))
+	static float GetScalableFloatValueAtLevel(const FScalableFloat& InScalableFloat, float InLevel = 1.f);
 };
